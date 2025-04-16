5ca7ee0a8630ffb2d085a1cefcfb8266c314361b
noyabr84 <noyabr.84@yandex>
2025-04-16 10:35:01 +0300

    После изменений

---
+++ BulletsCreator.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BulletsCreator : MonoBehaviour
+{
+   [SerializeField] private float _velocity;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private float _shootingDelay;
+    [SerializeField] private Transform _aim;
+
+    private Coroutine _coroutine;
+
+    private void Start()
+    {
+        _coroutine = StartCoroutine(ShootingWorker());
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(_coroutine);
+    }
+
+    private IEnumerator ShootingWorker()
+    {
+        Vector3 direction;
+        var wait = new WaitForSeconds(_shootingDelay);
+
+        while (true)
+        {
+            direction = (_aim.position - transform.position).normalized;
+            var bullet = Instantiate(_prefab, transform.position + direction, Quaternion.identity);
+            Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+            bulletRigidbody.transform.up = direction;
+            bulletRigidbody.velocity = direction * _velocity;
+
+            yield return wait;
+        }
+    }
+}
---
+++ Mover.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _placespoints;
+
+    private Transform[] _places;
+    private int _currentPlace = 0;
+
+    private void Start()
+    {
+        _places = new Transform[_placespoints.childCount];
+
+        for (int i = 0; i < _placespoints.childCount; i++)
+            _places[i] = _placespoints.GetChild(i).GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        Vector3 _place = _places[_currentPlace].position;
+        transform.position = Vector3.MoveTowards(transform.position, _place, _speed * Time.deltaTime);
+
+        if (transform.position == _place)
+            NextPlaceTakerLogic();
+    }
+
+    private void NextPlaceTakerLogic()
+    {
+        _currentPlace++;
+
+        if (_currentPlace == _places.Length)
+            _currentPlace = 0;
+
+        Vector3 thisPointPosition = _places[_currentPlace].transform.position;
+        transform.forward = thisPointPosition - transform.position;
+    }
+}

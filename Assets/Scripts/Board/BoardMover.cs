4aabf46878cdc6dd43cfc7907e170dc134bc677d
noyabr84 <noyabr.84@yandex>
2025-11-25 10:15:42 +0300

    delete coroutine from BoardMover

--- Assets/Scripts/Board/BoardMover.cs
+++ Assets/Scripts/Board/BoardMover.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -11,8 +10,8 @@ public class BoardMover : MonoBehaviour
     [SerializeField] private float _maxRotationZ = 35;
 
     private Rigidbody2D _rigidbody2D;
-    private Coroutine _coroutine;
-
+    private Quaternion _targetRotation;
+    
     public float Speed => _speed;
 
     private void Awake()
@@ -20,37 +19,18 @@ public class BoardMover : MonoBehaviour
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    private void OnDisable()
-    {
-        if (_coroutine != null)
-            StopCoroutine(_coroutine);
-    }
-
     public void Move()
     {
-        if (_coroutine != null)
-            StopCoroutine(_coroutine);
-
         _rigidbody2D.linearVelocity = new Vector3(_speed, _tapForce, 0);
         transform.rotation = Quaternion.Euler(0, 0, _maxRotationZ);
-
-        _coroutine = StartCoroutine(Rotate());
     }
 
-    private IEnumerator Rotate()
+    private void Update()
     {
-        Quaternion targetRotation;
-
-        while (transform.rotation.eulerAngles.z > _minRotationZ)
+        if (transform.rotation.eulerAngles.z > _minRotationZ)
         {
-            targetRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, _minRotationZ), _rotationSpeed * Time.deltaTime);
-            transform.rotation = targetRotation;
-
-            yield return null;
+            _targetRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, _minRotationZ), _rotationSpeed * Time.deltaTime);
+            transform.rotation = _targetRotation;
         }
-
-        StopCoroutine(_coroutine);
-
-        _coroutine = null;
     }
 }
